54253d0062bf4f66ef6a85a4469a7acc8cd3f858
zhangzh20 <1123598684@qq.com>
2022-07-20 00:58:10 +0800

    first commit from idea

---
+++ src/main/java/com/edu/controller/BookController.java
@@ -0,0 +1,69 @@
+package com.edu.controller;
+
+import com.edu.pojo.Books;
+import com.edu.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+
+    @RequestMapping("/toAddBook")
+    public String toAddPaper() {
+        return "addBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model, int id) throws UnsupportedEncodingException {
+        Books books = bookService.queryBookById(id);
+        System.out.println(books.getBookName());
+//        String bookNameUnicode = new String(books.getBookName().getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+//        String bookNameGBK = new String(bookNameUnicode.getBytes("GBK"), "GBK");
+//        System.out.println(bookNameGBK);
+//        books.setBookName(bookNameGBK);
+        System.out.println(books);
+        model.addAttribute("book",books );
+        return "updateBook";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(Model model, Books book) {
+        System.out.println(book);
+        bookService.updateBook(book);
+        Books books = bookService.queryBookById(book.getBookID());
+        model.addAttribute("books", books);
+        return "redirect:/book/allBook";
+    }
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+}
